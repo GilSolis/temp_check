6b7a40156a1219bb41f3f0563b3a5566321ef0a6
Gil Solis <gilsolisuw@gmail.com>
2020-12-09 20:14:50 -0800

    function alerting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+// on click  get value from input
 
+// do maths
+
+// return results
+
+
+document.querySelector('#btn').addEventListener('click', convert)
+
+function convert() {
+
+    celcius = document.querySelector('#celsius').value
+    if (!celcius) {
+        alert("Please enter your temp in Celcius")
+        return
+    }
+    fahrenheit = (celcius * 1.8) + 32
+    alert(fahrenheit)
+}
